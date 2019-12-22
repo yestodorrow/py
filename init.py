a89c0fa79e2fe922b170563d70bd4a1c09eb4946
刘刚(liugang02) <liugang02@qding.me>
2019-12-22 09:07:17 +0800

    varient

---
+++ init.py
@@ -0,0 +1,33 @@
+print("hello, Python");
+if True:
+    print("true")
+else:
+    print("flase")
+
+# 需要连字符的情况
+item_one=1
+item_two=2
+item_three=3
+total = item_one+\
+        item_two+\
+        item_three
+print(total)
+
+
+
+item_one="hello"
+item_two=","
+item_three="python"
+total = item_one+\
+        item_two+\
+        item_three
+print(total)
+
+
+# 不需要连字符的情况
+days=["Monday","Tuesday","Wednesday","Thursday",
+    "Friday"]
+print(days)
+
+name="this is a comment" # this is a comment
+
